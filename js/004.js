520750aafa60e459accd02faac15182ef865ad64
Unai Bolivar <ujbolivar@gmail.com>
2021-02-18 20:49:49 +0100

    004 revised

--- js/004.js
+++ js/004.js
@@ -10,15 +10,8 @@ function mxdiflg(a1, a2) {
     if (a1.length === 0 || a2.length === 0) return -1
     let l1 = a1.map(str => str.length)
     let l2 = a2.map(str => str.length)
-    let product1 = Math.max(...l1)
-    let product2 = Math.max(...l2)
-    let final = product1 - product2
-    if (final < 0) {
-        return product2 - product1
-    } else {
-        return final
-    }
-}
+    return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+  }
 
 //console.log(mxdiflg(["w", "dd"], ["a", "bef", "c"])) // --> 1
 let s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
