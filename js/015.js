e27ea91810724f9f518d191239014d4237504dcc
Unai Bolivar <ujbolivar@gmail.com>
2021-03-21 18:36:34 +0100

    daily challenge 015

---
+++ js/015.js
@@ -0,0 +1,16 @@
+/*
+Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+kebabize('camelsHave3Humps') // camels-have-humps
+Notes:
+
+the returned string should only contain lowercase letters
+*/
+
+function kebabize(str) {
+    return str
+      .replace(/[A-Z]/g, (match) => '-' + match.toLowerCase())
+  }
+
+  console.log(kebabize('camelsHave3Humps'))
