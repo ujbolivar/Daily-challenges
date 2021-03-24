c740f0a1bff4951d076d2dc2e9736be6180f6421
Unai Bolivar <ujbolivar@gmail.com>
2021-03-24 23:16:04 +0100

    daily code 017

---
+++ js/017.js
@@ -0,0 +1,23 @@
+/*
+
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+
+! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+
+Examples:
+iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+
+*/
+
+
+function iqTest(numbers){
+    var nums = numbers.split(" ").map(x => x % 2);  
+    var sum = nums.reduce((a,b) => a + b);  
+    var target = sum > 1 ? 0 : 1;
+    
+    return nums.indexOf(target) + 1;
+  }
+
+  console.log(iqTest("2 4 7 8 10"));
