28e9ded8847d7a76236c6d0dd90661b816aa9bd5
Unai Bolivar <ujbolivar@gmail.com>
2021-03-29 23:32:30 +0200

    daily challenge 019

---
+++ js/019.js
@@ -0,0 +1,13 @@
+/*
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+
+*/
+
+function solution(str){
+    return str.split("").reverse().join("");
+}
+
+console.log(solution("unai"));
