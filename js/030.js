6f24f10b6fa9aa40a2edbb7945fa9ab66f09e1d8
Unai Bolivar <ujbolivar@gmail.com>
2021-05-25 21:08:54 +0200

    daily challenge 030

---
+++ js/030.js
@@ -0,0 +1,20 @@
+/*
+
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+*/
+
+function bmi(weight, height) {
+    let bmi = weight / (height ** 2)
+    return bmi <= 18.5 ? "Underweight" : bmi <= 25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese"
+  }
+
+console.log(bmi(80, 1.8));
