cf1c9ff597f08397abac244f082fb62d1abf89f2
Mini02 <Mini02@Mac-mini-de-Intereconomia.local>
2021-06-01 16:47:11 +0200

    daily challenge 032

---
+++ 033.js
@@ -0,0 +1,24 @@
+/*
+
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+
+rgb(255, 255, 255) // returns FFFFFF
+rgb(255, 255, 300) // returns FFFFFF
+rgb(0,0,0) // returns 000000
+rgb(148, 0, 211) // returns 9400D3
+
+*/
+
+function rgb(r, g, b){
+    // complete this function  
+  }
+
+
+console.log(rgb(255, 255, 255))
+console.log(rgb(255, 255, 300))
+console.log(rgb(0,0,0))
+console.log(rgb(148, 0, 211))
