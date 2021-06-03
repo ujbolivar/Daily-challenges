316e1715172511452d8bde4ec54a648539b229cf
Mini02 <Mini02@Mac-mini-de-Intereconomia.local>
2021-06-03 14:15:26 +0200

    got it working but need to figure out the bugs

--- js/033.js
+++ js/033.js
@@ -15,37 +15,45 @@ rgb(148, 0, 211) // returns 9400D3
 
 function rgb(r, g, b){
     let hexValues = {
-        1: 1,
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 5,
-        6: 6
-        A: 10,
-        B: 11,
-        C: 12,
-        D: 13,
-        E: 14,
-        F: 15
+        "0": "0",
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9",
+        "A": "10",
+        "B": "11",
+        "C": "12",
+        "D": "13",
+        "E": "14",
+        "F": "15"
     }
+
     let rgbArray = [r, g, b]
     
-    let answer = "#";
+    let answer = ["#"];
 
     function getKeyByValue(object, value) {
         return Object.keys(object).find(key => object[key] === value);
+        
       }
 
       let firstDigit;
       let secondDigit;
 
-    rgbArray.map(value => {
-        let result = value / 16;
+    rgbArray.map(number => {
+        let result = number / 16;
         firstDigit = result.toString().substr(0, 2);
-        secondDigit = (Number('0.' + result.toString().substr(3, 2)) * 16).toString().substr(0, 2);
+        secondDigit = Math.round(Number('0.' + result.toString().substr(3, 2)) * 16).toString().substr(0, 2);
+        answer.push(getKeyByValue(hexValues, firstDigit));
+        answer.push(getKeyByValue(hexValues, secondDigit));
+
     })
-    
-        return answer
+    return answer.join("")
     
 
 }
