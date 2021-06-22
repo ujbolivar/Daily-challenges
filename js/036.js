62b1e6011878edc6aae97db4cc8a229a3faf9cf7
Unai Bolivar <ujbolivar@gmail.com>
2021-06-22 22:09:32 +0200

    daily challenge 036

---
+++ js/036.js
@@ -0,0 +1,51 @@
+/*
+
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+*/
+
+const quarterOf = (month) => {
+
+    switch (month) {
+        case 1:
+            return 1;
+            break;
+        case 2:
+            return 1;
+            break;
+        case 3:
+            return 1;
+            break;
+        case 4:
+            return 2;
+            break;
+        case 5:
+            return 2;
+            break;
+        case 6:
+            return 2;
+            break;
+        case 7:
+            return 3;
+            break;
+        case 8:
+            return 3;
+            break;
+        case 9:
+            return 3;
+            break;
+        case 10:
+            return 4;
+            break;             
+        case 11:
+            return 4;
+            break;
+        case 12:
+            return 4;       
+        default:
+            break;
+    }
+  
+}
